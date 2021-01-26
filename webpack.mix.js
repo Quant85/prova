88544b553ee97042c41844bc367e9205e3df2327
Quant85 <ant.quattrocchi@stud.uniroma3.it>
2021-01-26 18:39:29 +0100

    prova push

---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+//webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix
+  .js('resources/js/app.js', 'js').vue({ version: 3 })
+  .sass('resources/sass/main.scss', 'css').copy(
+        'node_modules/@fortawesome/fontawesome-free/webfonts',
+        'public/webfonts'
+    ).setPublicPath('public');
